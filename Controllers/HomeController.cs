18efce5c46b057b717ca11df33e62baf22eaa1f8
fkrsyf29 <fakhri.syafrullah@gmail.com>
2022-10-14 17:36:19 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace FrontEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Location> locations = GetLocation();
+            return View(locations);
+        }
+
+        private static List<Location> GetLocation()
+        {
+            List<Location> location = new List<Location>();
+            string apiUrl = "https://localhost:5401/api/SkillTest/getLocation";
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(apiUrl).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                location = JsonConvert.DeserializeObject<List<Location>>(response.Content.ReadAsStringAsync().Result);
+            }
+
+            return location;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Class.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontEnd.Models
+{
+    public class Location
+    {
+        public string location_id { get; set; }
+        public string location_name { get; set; }
+    }
+}
